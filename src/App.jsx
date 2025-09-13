24b3c81f5313bdfc75fb303a86397ce1879057e6
pehesara112455 <ar112455@fhss.sjp.ac.lk>
2025-09-13 15:30:34 +0530

    Completed navabar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home"
+import Navbar from './Components/Navbar';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <hi>wellcome my mobile store</hi>
+    <Router>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/Navbar' element  = {<Navbar/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { NavLink } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+              <Navbar />
+
+    </div>
+      
+  );
+}
+
+export default Home;
